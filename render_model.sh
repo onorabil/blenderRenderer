a3f4fd6886be306e18e0f33590cb3b9cd3d9969d
Alexandru Jercan <jercan_alex27@yahoo.com>
2020-12-10 14:19:57 +0200

    fixed scripts

--- render_model.sh
+++ render_model.sh
@@ -3,4 +3,4 @@
 model_name=$1
 materials=$(ls poliigon_material_samples)
 
-for m in $materials ; do ./run.sh $model_name $m ; done
+for m in $materials ; do ./apply_texture.sh $model_name $m ; done
