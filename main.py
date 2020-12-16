fd28a3c35701082dec83a086b6bc8b306d57d74b
Alexandru Jercan <jercan_alex27@yahoo.com>
2020-12-16 10:52:02 +0200

    small changes

--- main.py
+++ main.py
@@ -123,15 +123,7 @@ def getVisibleObjVerticesAndEdges(scene, obj):
 
     edges = []
     for i, edge in enumerate(obj.data.edges):
-        # if not edge.select or not obj.data.vertices[edge[0]].select or not obj.data.vertices[edge[1]].select:
-        #     continue
-        # print(obj.data.vertices[edge[0]].select)
-        # print('e0', edge.vertices[0])
-        #for prop in edge:
-        #    print(prop)
-        # print('e1', edge.vertices[1])
         if not edge.select and (obj.data.vertices[edge.vertices[0]].select and obj.data.vertices[edge.vertices[1]].select):
-            #continue
             new_v1, new_v2 = mapVertices[edge.vertices[0]], mapVertices[edge.vertices[1]]
             edges.append((new_v1, new_v2))
 
@@ -187,7 +179,7 @@ def select_visible_vertices(scene, obj):
                 obj.data.vertices[i].select = True
     return obj
 
-def getDistancesToBBox(cam, scene, BBox):
+def getDistancesToBBox(scene, BBox):
     MinBBox, MaxBBox = BBox
     distancesDict = {}
     meshesDict = {}
@@ -203,43 +195,30 @@ def getDistancesToBBox(cam, scene, BBox):
         print('selected edges', len(edges))
         if len(vertices) == 0 or len(edges) == 0:
             continue
-        # assert len(vertices) > 0 and len(edges) > 0
-        # vertices, edges = getObjVerticesAndEdges(item)
         allVertices = np.concatenate([allVertices, vertices], axis=0)
         allEdges = np.concatenate([allEdges, edges], axis=0)
-        # MinBBox, MaxBBox = np.min(allVertices, axis=0), np.max(allVertices, axis=0)
     return allVertices, allEdges
 
-def render_scene(camera, baseDir, numViews, outputs, BBox, model_identifier):
-    scene = bpy.context.scene
+def render_scene(scene, cameraRig, baseDir, numViews, outputs, BBox, model_identifier):
     scene.render.image_settings.file_format = 'PNG'
     views_x, views_y, views_z = numViews
     stepsize_x, stepsize_y, stepsize_z = 360 // views_x, 360 // views_y, 360 // views_z
 
-    # print(list(scene.objects))
-    # return
-
     # TODO
     cameraWTF = scene.objects['Camera']
 
-    # old = blockPrint()
-    # bpy.ops.render.render(write_still=False)
-    # enablePrint(old)
-
     for i in range(views_x):
         angle_x, rad_x = stepsize_x * i, radians(stepsize_x * i)
-        camera.rotation_euler[0] = rad_x
+        cameraRig.rotation_euler[0] = rad_x
         for j in range(views_y):
             angle_y, rad_y = stepsize_y * j, radians(stepsize_y * j)
-            camera.rotation_euler[1] = rad_y
+            cameraRig.rotation_euler[1] = rad_y
             for k in range(views_z):
                 angle_z, rad_z = stepsize_z * k, radians(stepsize_z * k)
-                camera.rotation_euler[2] = rad_z
-                # scene.update()
+                cameraRig.rotation_euler[2] = rad_z
+
                 dg = bpy.context.evaluated_depsgraph_get()
                 dg.update()
-                #bpy.ops.wm.redraw_timer(type='DRAW_WIN_SWAP', iterations=1)
-                # time.sleep(3)
                 scene.render.filepath = os.path.join(baseDir, model_identifier + "_render_%d_%d_%d" % (angle_x, angle_y, angle_z))
                 outputs["depth"].file_slots[0].path = scene.render.filepath + "_depth.exr"
                 # normal_file_output.file_slots[0].path = scene.render.filepath + "_normal.exr"
@@ -260,37 +239,29 @@ def render_scene(camera, baseDir, numViews, outputs, BBox, model_identifier):
                 inv_projection_matrix = projection_matrix.copy()
                 inv_projection_matrix.invert()
 
-                # print(modelview_matrix, projection_matrix)
-
                 # TODO make an output interface
                 pklFile = open(scene.render.filepath + '.pkl', "wb")
-                # objFile = open(scene.render.filepath + '.obj', "w")
 
                 transformedVertices = np.zeros(allVertices.shape)
 
-                # objFile.write('# vertices\n')
                 idx = 0
                 for vertex in allVertices:
                     vertex = projection_matrix @ inv_modelview_matrix @ Vector((vertex[0], vertex[1], vertex[2], 1))
                     vertex /= vertex.w
                     transformedVertices[idx, :] = vertex[:-1]
-                    # objFile.write('v {0:.7f} {1:.7f} {2:.7f}\n'.format(vertex[0], vertex[1], vertex[2]))
                     idx += 1
-
-                # objFile.write('# faces\n')
 
                 pickle.dump(transformedVertices, pklFile)
                 pickle.dump(allEdges.astype(np.int), pklFile)
-                # objFile.close()
                 pklFile.close()
                 # TODO
 
-                vertices, edges = getDistancesToBBox(camera, scene, BBox)
+                vertices, edges = getDistancesToBBox(scene, BBox)
                 print("Rotation. X:(%d, %2.2f), Y:(%d, %2.2f), Z:(%d, %2.2f). Vertices: %d. Edges: %d" % \
                     (angle_x, rad_x, angle_y, rad_y, angle_z, rad_z, len(vertices), len(edges)))
 
                 old = blockPrint()
-                bpy.ops.render.render(write_still=True)  # render still
+                bpy.ops.render.render(write_still=True)
                 enablePrint(old)
 
 if __name__ == "__main__":
@@ -301,15 +272,13 @@ if __name__ == "__main__":
     links = tree.links
 
     # material path
-    MATERIAL_NAME = args.material #'TilesMarbleSageGreenBrickBondHoned001_3K'
+    MATERIAL_NAME = args.material
     MATERIAL_PATH = os.path.join(current_script_path, MATERIAL_NAME)
     
     # render w/ cycles
     bpy.context.scene.render.engine = 'BLENDER_EEVEE'
 
     # Add passes for additionally dumping albedo and normals.
-    #bpy.context.scene.render.layers["RenderLayer"].use_pass_normal = True
-    #bpy.context.scene.render.layers["RenderLayer"].use_pass_color = True
     bpy.types.RenderLayer.use_pass_normal = True
     bpy.types.RenderLayer.use_pass_color = True
     bpy.context.scene.render.image_settings.file_format = "OPEN_EXR"
@@ -325,76 +294,42 @@ if __name__ == "__main__":
     # Depth setup
     depth_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
     depth_file_output.label = 'Depth Output'
+    depth_file_output.base_path = ""
     links.new(render_layers.outputs['Depth'], depth_file_output.inputs[0])
 
     scale_normal = tree.nodes.new(type="CompositorNodeMixRGB")
     scale_normal.blend_type = 'MULTIPLY'
-    # scale_normal.use_alpha = True
     scale_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 1)
     links.new(render_layers.outputs['Normal'], scale_normal.inputs[1])
 
     bias_normal = tree.nodes.new(type="CompositorNodeMixRGB")
     bias_normal.blend_type = 'ADD'
-    # bias_normal.use_alpha = True
     bias_normal.inputs[2].default_value = (0.5, 0.5, 0.5, 0)
     links.new(scale_normal.outputs[0], bias_normal.inputs[1])
 
     normal_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
     normal_file_output.label = 'Normal Output'
+    normal_file_output.base_path = ""
     links.new(bias_normal.outputs[0], normal_file_output.inputs[0])
 
     albedo_file_output = tree.nodes.new(type="CompositorNodeOutputFile")
     albedo_file_output.label = 'Albedo Output'
-    #for x in render_layers.outputs:
-    #    print(x)
-    #links.new(render_layers.outputs['Color'], albedo_file_output.inputs[0])
+    albedo_file_output.base_path = ""
     links.new(render_layers.outputs['DiffCol'], albedo_file_output.inputs[0])
 
     # Delete default cube
-    #bpy.data.objects['Cube'].select = True
     bpy.data.objects['Cube'].select_set(state=True)
     bpy.ops.object.delete()
 
-    bpy.ops.import_scene.obj(filepath=args.obj)
-    for object in bpy.context.scene.objects:
-        if object.name in ['Camera', 'Light']:
-            continue
-        #bpy.context.scene.objects.active = object
-        object.select_set(state=True)
-        bpy.context.view_layer.objects.active = object
-        print('sel o', bpy.context.selected_objects[0])
-        #bpy.context.selected_objects(object)
-        # "Remove doubles"
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.remove_doubles()
-        bpy.ops.object.mode_set(mode='OBJECT')
-
-        # "Edge split"
-        bpy.ops.object.modifier_add(type='EDGE_SPLIT')
-        bpy.context.object.modifiers["EdgeSplit"].split_angle = 1.32645
-        bpy.ops.object.modifier_apply(modifier="EdgeSplit")
+    bpy.ops.import_scene.obj(filepath=args.obj)     
         
-        # add uv map
-        bpy.ops.object.editmode_toggle()
-        bpy.ops.mesh.select_all(action='SELECT') # for all faces
-        bpy.ops.uv.smart_project(angle_limit=66, island_margin = 0.02)
-        bpy.ops.object.editmode_toggle()
-        object.select_set(False)
-        #bpy.ops.mesh.uv_texture_add()
-        
-        
-
     # Make light just directional, disable shadows.
     light = bpy.data.lights['Light']
     light.type = 'SUN'
     light.cycles.cast_shadow = True
-    light.energy = 2  ######################################################## Do for the other lights
+    light.energy = 2
     light.use_nodes = True
     light.node_tree.nodes['Emission'].inputs[1].default_value = 2
-    # Possibly disable specular shading:
-    
-    ## HERE
-    ##light.use_specular = False
 
     # Add another light source so stuff facing away from light is not completely dark
     bpy.ops.object.light_add(type='SUN')
@@ -410,7 +345,6 @@ if __name__ == "__main__":
     scene.render.resolution_x = 600
     scene.render.resolution_y = 600
     scene.render.resolution_percentage = 100
-    ## scene.render.alpha_mode = 'TRANSPARENT'
     cam = scene.objects['Camera']
     cam.location = (0, 1, 0.6)
     cam_constraint = cam.constraints.new(type='TRACK_TO')
@@ -419,25 +353,11 @@ if __name__ == "__main__":
     b_empty = parent_obj_to_camera(cam)
     cam_constraint.target = b_empty
 
-    # bvh, vertices = BVHTreeAndVerticesInWorldFromObj( bpy.data.objects["mesh1_mesh1-geometry"]  )
-    # for v in vertices:
-    #     co2D = world_to_camera_view( scene, scene.camera, v )
-
-
-    # print(scene.camera.view_frame)
-    # print(cam.view_frame)
-    # print(b_empty.view_frame)
-
     model_identifier = os.path.split(os.path.split(args.obj)[0])[1]
     fp = os.path.join(current_script_path, args.output_folder)
-    # # for output_node in [depth_file_output, normal_file_output, albedo_file_output]:
-    #     # output_node.base_path = ''
     set_path = MATERIAL_PATH
     lmh = Load_Material_Helper()
-    status, poliigon_material = lmh.build_material_from_set(bpy.context, set_path)
-    #material_test = bpy.data.materials.get("RoadAsphaltWorn006_HIRES")
-
-    depth_file_output.base_path = ""
+    status, material = lmh.build_material_from_set(bpy.context, set_path)
 
     allEdges, allVertices = np.zeros((0, 2)), np.zeros((0, 3))
     meshesDict = {}
@@ -447,7 +367,7 @@ if __name__ == "__main__":
         print(item.name)
         #assign material
         item.data.materials.clear()
-        item.data.materials.append(poliigon_material)
+        item.data.materials.append(material)
         
         #need vertices selected here
         vertices, edges = getObjVerticesAndEdges(item)
@@ -457,29 +377,15 @@ if __name__ == "__main__":
         allVertices = np.concatenate([allVertices, vertices], axis=0)
         allEdges = np.concatenate([allEdges, edges], axis=0)
     MinBBox, MaxBBox = np.min(allVertices, axis=0), np.max(allVertices, axis=0)
-
-    #     print('vertices')
-    #     print(thisVertices)
-    #     print('separate vertices')
-    #     print('cm', modelview_matrix)
-    #     print('pm', projection_matrix)
-    #     for vertex in thisVertices:
-    #         print(vertex)
-    #         vertex_transformed = projection_matrix * modelview_matrix * Vector((vertex[0], vertex[1], vertex[2], 1))
-    #         vertex_transformed /= vertex_transformed.w
-    #         print(vertex_transformed)
     
     light_data = bpy.data.lights.new(name="additional_light", type='POINT')
     light_data.cycles.cast_shadow = True
     light_data.use_nodes = True
     light_data.node_tree.nodes['Emission'].inputs[1].default_value = np.random.randint(2)
     light_object = bpy.data.objects.new(name="additional_light", object_data=light_data)
-    ## scene.objects.link(light_object)
     bpy.context.collection.objects.link(light_object)
     light_object.location = (np.random.randint(2), np.random.randint(2), np.random.randint(2))
     
-    render_scene(camera=b_empty, baseDir=fp, numViews=(args.views_x, args.views_y, args.views_z),
+    render_scene(scene=bpy.context.scene, cameraRig=b_empty, baseDir=fp, numViews=(args.views_x, args.views_y, args.views_z),
                  outputs={"depth": depth_file_output}, BBox=(MinBBox, MaxBBox), model_identifier=model_identifier)
-
-#blender -b --python main.py -- models/alder_leaf/model_normalized.obj --material materials/GroundMoss001_3K --views_x=4 --views_y=4 --views_z=4 --output_folder output
 
